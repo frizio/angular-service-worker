7573b11085ebcf7461e482e9d688c6c71ed1c3b1
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-05-28 16:37:47 +0200

    Add Max's started code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Post } from './post.model';
+
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+
+  posts: Post[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.http
+      .get<Post[]>(POSTS_URL)
+      .subscribe(
+        fetchedPosts => (
+          this.posts = fetchedPosts)
+      );
+  }
+}
